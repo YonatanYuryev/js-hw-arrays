0cd343937857093eb63bd5c10f0d851b5eadbda9
Yonatan Yuryev <yonilight333@gmail.com>
2025-05-16 13:30:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Task 5
+// const fruits = ["apple", "banana", "cherry", "date"];
+// fruits.splice(1, 1, "blueberry", "blackberry")
+// console.log(fruits);
+
+// Task 6
+// const colors = ["red", "orange", "yellow", "green", "blue"];
+// colors.splice(0,1);
+// colors.splice(-2);
+// console.log(colors);
+
+// Task 7
+// const animals1 = ["cat", "dog"];
+// const animals2 = ["lion", "tiger"];
+// let allAnimals = [];
+// allAnimals = allAnimals.concat(animals1, animals2);
+// console.log(allAnimals);
+
+
+// Task 8
+// const numbers = [7, 3, 9, 1, 5];
+// console.log(numbers.sort((a,b) => a - b));
+
+
+// Task 9
+// const letters = ["a", "b", "c", "d"];
+// console.log(letters.reverse());
+
+
+// Task 10 
+// const sentence = "JavaScript is awesome";
+// console.log(sentence.split(' '));
+
+
+// Task 11
+// const words = ["Hello", "world"];
+// let string = words.join(' ');
+// console.log(string);
